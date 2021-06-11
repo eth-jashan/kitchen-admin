af6f31a130c99ee02e401cd7be8e7d9c09090e92
Prafulkumar <aruparayilprpon19ce@student.mes.ac.in>
2021-06-11 16:33:49 +0530

    merge

--- source/screen/DishUploadScreen.js
+++ source/screen/DishUploadScreen.js
@@ -7,25 +7,19 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 const DishUploadScreen = (props) => {
     const[name,setName]=useState()
     const[description,setDescription]=useState()
-    const[img,setImg]=useState([])
-    const SLIDER_WIDTH = Dimensions.get('screen').width;
-    const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.7);
-
+    const[img,setImg]=useState()
+    const[quantity,setQuantity]=useState()
+    const[price,setPrice]=useState()
+    const[serve,setServe]=useState()
+    //const SLIDER_WIDTH = Dimensions.get('screen').width;
+    //const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.7);
+    
     return(
         <SafeAreaView style={{flex:1}} >
-            <View style={{height:Dimensions.get('screen').height*0.4,padding:20,backgroundColor:'#ffffff'}}>
-                {img?<View style={{flexDirection:'row'}}>
-                    <Pressable style={{ backgroundColor:'#08818a', padding:8, borderRadius:8, alignSelf:'center', justifyContent:'center'}} >
-                        <Text style={{fontFamily:'book', fontSize:24, alignSelf:'center', color:'white'}} >Add More Images</Text>
-                    </Pressable>
-                </View>:
-                <View style={{alignSelf:'center'}} >
-                <Pressable style={{ backgroundColor:'#08818a', padding:8, borderRadius:8, alignSelf:'center', justifyContent:'center'}} >
-                <Text style={{fontFamily:'book', fontSize:24, alignSelf:'center', color:'white'}} >Add Dish Image</Text>
-                </Pressable>
-                </View>}
+            <View style={{backgroundColor:'#ffffff'}}>
+                
             </View>
-            <View style={{marginTop:10,borderTopLeftRadius:20,borderTopRightRadius:20}} >
+            <View style={{marginTop:10,borderTopLeftRadius:20,borderTopRightRadius:20,backgroundColor:'#D3D3D3'}} >
                 <ScrollView>
                 <View style={{marginVertical:5, alignSelf:'center'}} >
                 <TextInput
@@ -39,28 +33,67 @@ const DishUploadScreen = (props) => {
                 </View>
                 <View style={{marginVertical:10, alignSelf:'center'}} >
                 <TextInput
+                    multiline
                     value={description}
                     onChangeText={setDescription}
                     mode='flat'
                     label="Dish Description"
                     theme ={{colors:{primary:'#08818a',underlineColor:'transparent'}}}
-                    style={{ fontFamily: 'medium', fontColor: '#08818a', height: 70, width: Dimensions.get('screen').width*0.95, alignSelf:'center' }}
+                    style={{ fontFamily: 'medium', fontColor: '#08818a', width: Dimensions.get('screen').width*0.95, alignSelf:'center' }}
                 />
                 </View>
                 <View style={{flexDirection:'row'}} >
                 <View style={{margin:5}} >
-
+                <TextInput
+                    value={quantity}
+                    onChangeText={setQuantity}
+                    mode='flat'
+                    keyboardType='number-pad'
+                    label='Quantity'
+                    theme ={{colors:{primary:'#08818a',underlineColor:'transparent'}}}
+                    style={{ fontFamily: 'medium', fontColor: '#08818a',height:70, width: Dimensions.get('screen').width*0.15, alignSelf:'center' }}
+                />
                 </View>
                 <View style={{margin:5}} >
-
+                <TextInput
+                    value={price}
+                    onChangeText={setPrice}
+                    mode='flat'
+                    label='Price'
+                    keyboardType='number-pad'
+                    theme ={{colors:{primary:'#08818a',underlineColor:'transparent'}}}
+                    style={{ fontFamily: 'medium', fontColor: '#08818a',height:70, width: Dimensions.get('screen').width*0.55, alignSelf:'center' }}
+ 
+                />
                 </View>
                 </View>
                 <View style={{flexDirection:'row'}} >
+                    <View style={{margin:5}} >
+                    <Text>Maximum Serves: </Text>
+                    </View>
+                    <View style={{margin:5}} >
+                    <TextInput
+                        value={price}
+                        onChangeText={setPrice}
+                        mode='flat'
+                        label='Price'
+                        keyboardType='number-pad'
+                        theme ={{colors:{primary:'#08818a',underlineColor:'transparent'}}}
+                        style={{ fontFamily: 'medium', fontColor: '#08818a',height:70, width: Dimensions.get('screen').width*0.45, alignSelf:'center' }}
+ 
+                    />
+                    </View>
+                </View>
+                <View style={{flexDirection:'row'}} >
                 <View style={{margin:5}} >
-
+                <Pressable onPress={}  style={{ backgroundColor:'#08818a', padding:8, borderRadius:8, width:'40%', alignSelf:'center', justifyContent:'center'}} >
+                <Text style={{fontFamily:'book', fontSize:24, alignSelf:'center', color:'white'}} >Pre-Order</Text>
+                </Pressable>
                 </View>
                 <View style={{margin:5}} >
-
+                <Pressable  style={{ backgroundColor:'#08818a', padding:8, borderRadius:8, width:'40%', alignSelf:'center', justifyContent:'center'}} >
+                <Text style={{fontFamily:'book', fontSize:24, alignSelf:'center', color:'white'}} >On-Demand</Text>
+                </Pressable>
                 </View>
                 </View>
                 <View style={{width:'100%'}} >
