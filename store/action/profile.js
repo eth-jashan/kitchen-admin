723b732a22095e102ddd91b698d9d11b83b95d7d
joshipiyush9969 <piyushjb7@hotmail.com>
2021-06-16 15:57:52 +0530

    branching solved in rtd

--- store/action/profile.js
+++ store/action/profile.js
@@ -18,7 +18,7 @@ export const addCuisine = (name) => {
 export const createAccount=(name,email,phone, uid, token)=>{
     return async(dispatch,getState)=>{
         
-        const response=await fetch(`https://mineral-concord-314020-default-rtdb.asia-southeast1.firebasedatabase.app/chef/${uid}/profile.json?`,{
+        const response=await fetch(`https://mineral-concord-314020-default-rtdb.asia-southeast1.firebasedatabase.app/chef/profile.json?`,{
             method:'POST',
             headers:{'Content-Type':'application\json'},
             body:JSON.stringify({
@@ -49,7 +49,7 @@ export const accountSetup=(cuisine, type, geoAddress, house, landmark, pincode, 
         const id = getState().profile.profileId
         const uid = getState().profile.uid
 
-        await fetch(`https://mineral-concord-314020-default-rtdb.asia-southeast1.firebasedatabase.app/chef/${uid}/profile/${id}.json?`,{
+        await fetch(`https://mineral-concord-314020-default-rtdb.asia-southeast1.firebasedatabase.app/chef/profile/${id}.json?`,{
                 method:'PATCH',
                 headers:{'Content-Type':'application\json'},
                 body:JSON.stringify({
@@ -87,19 +87,19 @@ export const addKyc = (name,phone,adharURI,adharNo,fssiURI,fssiNo,panURI,panNo) 
         const fssiBlob = await fssi.blob();
         const panBlob = await pan.blob();
 
-        const adharRef = storage.ref(`${'kyc/'}${uid}${'/'}${'adhar'}`);
-        const fssiRef = storage.ref(`${'kyc/'}${uid}${'/'}${'fssi'}`);
-        const panRef = storage.ref(`${'kyc/'}${uid}${'/'}${'pan'}`);
+        const adharRef = storage().ref(`${'kyc/'}${uid}${'/'}${'adhar'}`);
+        const fssiRef = storage().ref(`${'kyc/'}${uid}${'/'}${'fssi'}`);
+        const panRef = storage().ref(`${'kyc/'}${uid}${'/'}${'pan'}`);
         
         await adharRef.put(adharBlob);
         await fssiRef.put(fssiBlob);
         await panRef.put(panBlob);
 
-        const adharUrl = await storage.ref(`${'kyc/'}${uid}${'/'}${'adhar'}`).getDownloadURL();
-        const fssiUrl = await storage.ref(`${'kyc/'}${uid}${'/'}${'fssi'}`).getDownloadURL();
-        const panUrl = await storage.ref(`${'kyc/'}${uid}${'/'}${'pan'}`).getDownloadURL();
+        const adharUrl = await storage().ref(`${'kyc/'}${uid}${'/'}${'adhar'}`).getDownloadURL();
+        const fssiUrl = await storage().ref(`${'kyc/'}${uid}${'/'}${'fssi'}`).getDownloadURL();
+        const panUrl = await storage().ref(`${'kyc/'}${uid}${'/'}${'pan'}`).getDownloadURL();
 
-        const response  = await fetch(`https://mineral-concord-314020-default-rtdb.asia-southeast1.firebasedatabase.app/Chef/${id}/details.json?`,{
+        const response  = await fetch(`https://mineral-concord-314020-default-rtdb.asia-southeast1.firebasedatabase.app/chef/kyc.json?`,{
             method:'POST',
             headers:{'Content-Type':'application\json'},
             body:JSON.stringfy({
